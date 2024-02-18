96d2c74b997d7db367d5cec0a1f9ce8670e9c980
Geoffreey <geoffrey.torres64@gmail.com>
2024-02-18 11:49:25 -0600

    Se agregan funciones de seleccion de imput por ID de mascota

--- JavaScript/proyecto2/mokepon.js/mokepon.js
+++ JavaScript/proyecto2/mokepon.js/mokepon.js
@@ -1,5 +1,22 @@
-function seleccionarMascotaJugador() {
-  alert("SELECCIONASTE TU MASCOTA");
+function iniciarJuego() {
+  let botonMascotaJugador = document.getElementById("boton-mascota");
+  botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
 }
-let botonMascotaJugador = document.getElementById("boton-mascota");
-botonMascotaJugador.addEventListener("click", seleccionarMascotaJugador);
+
+function seleccionarMascotaJugador() {
+  let inputHipodoge = document.getElementById("hipodoge");
+  let inputCapipepo = document.getElementById("capipepo");
+  let inputRatigueya = document.getElementById("ratigueya");
+
+  if (inputHipodoge.checked) {
+    alert("SELECCIONASTE A HIPODOGE");
+  } else if (inputCapipepo.checked) {
+    alert("SELECCIONASTE A CAPIPEPO");
+  } else if (inputRatigueya.checked) {
+    alert("SELECCIONASTE A RATIGUEYA");
+  } else {
+    alert("SELECCIONA UNA MASCOTA");
+  }
+}
+
+window.addEventListener("load", iniciarJuego);
